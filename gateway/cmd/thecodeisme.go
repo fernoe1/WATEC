25ebbf3665bec5b0f30a4e8f73e3fd9511263854
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-13 10:40:43 +0500

    feat(gateway): telemetry

--- gateway/cmd/thecodeisme.go
+++ gateway/cmd/thecodeisme.go
@@ -1,4 +1,41 @@
 package main
 
+import (
+	"context"
+	"log"
+	"log/slog"
+
+	"github.com/fernoe1/WATEC/gateway/config"
+	"github.com/fernoe1/WATEC/gateway/pkg/telemetry"
+	"go.opentelemetry.io/contrib/bridges/otelslog"
+	"go.opentelemetry.io/otel"
+)
+
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	slog.Info("reading config")
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	slog.Info("initializing otel providers")
+	tp, mp, lp, err := telemetry.Init(ctx, cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		tp.Shutdown(ctx)
+		mp.Shutdown(ctx)
+		lp.Shutdown(ctx)
+	}()
+
+	tracer := otel.Tracer(cfg.Telemetry.Name)
+
+	meter := mp.Meter(cfg.Telemetry.Name)
+
+	handler := otelslog.NewHandler(cfg.Telemetry.Name)
+	logger := slog.New(handler)
+
 }
--- gateway/config/config.go
+++ gateway/config/config.go
@@ -1,1 +1,52 @@
 package config
+
+import (
+	"strings"
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Http
+	Telemetry
+}
+
+type Http struct {
+	Port              string
+	PprofPort         string
+	ReadHeaderTimeout time.Duration
+	ReadTimeout       time.Duration
+	WriteTimeout      time.Duration
+	IdleTimeout       time.Duration
+	MaxHeaderBytes    int
+}
+
+type Telemetry struct {
+	Name     string
+	Endpoint string
+}
+
+func ReadConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.AddConfigPath("../config")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	if !strings.HasPrefix(cfg.Http.Port, ":") {
+		cfg.Http.Port = ":" + cfg.Http.Port
+	}
+
+	if !strings.HasPrefix(cfg.Http.PprofPort, ":") {
+		cfg.Http.PprofPort = ":" + cfg.Http.PprofPort
+	}
+
+	return &cfg, nil
+}
