db43b21588569ff2265957e74ea8ef4efb88dbde
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-14 09:18:43 +0500

    chore(gateway)

--- gateway/internal/application/adapter/http/handler/route.go
+++ gateway/internal/application/adapter/http/handler/route.go
@@ -4,19 +4,19 @@ func (c *ClassroomHandler) MapClassroomRoutes() {
 	c.group.POST("", c.Create)
 	c.group.GET("", c.Read)
 	c.group.PUT("", c.Update)
-	c.group.DELETE(":roomNumber", c.Delete)
+	c.group.DELETE("/:roomNumber", c.Delete)
 }
 
 func (l *LockerHandler) MapLockerRoutes() {
 	l.group.POST("", l.Create)
-	l.group.GET(":number", l.Read)
+	l.group.GET("/:number", l.Read)
 	l.group.PUT("", l.Update)
 	l.group.DELETE(":number", l.Delete)
 }
 
 func (t *TeacherHandler) MapTeacherRoutes() {
 	t.group.POST("", t.Create)
-	t.group.GET(":name", t.Create)
+	t.group.GET("/:name", t.Create)
 	t.group.PUT("", t.Update)
-	t.group.DELETE(":name", t.Delete)
+	t.group.DELETE("/:name", t.Delete)
 }
