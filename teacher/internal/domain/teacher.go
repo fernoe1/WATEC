15170e3a4d4994b298434213c6a89ea60a7364c7
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-14 11:35:09 +0500

    feat(teacher): fk fix

--- teacher/internal/domain/teacher.go
+++ teacher/internal/domain/teacher.go
@@ -2,12 +2,13 @@ package domain
 
 type Teacher struct {
 	Name string `gorm:"primaryKey"`
-	Free []Free `gorm:"foreignKey:RoomNumber;references:RoomNumber;constraint:OnDelete:CASCADE"`
+	Free []Free `gorm:"foreignKey:TeacherName;references:Name;constraint:OnDelete:CASCADE"`
 }
 
 type Free struct {
-	ID         int64 `gorm:"primaryKey"`
-	RoomNumber int64 `gorm:"index"`
-	From       int64
-	To         int64
+	ID          int64  `gorm:"primaryKey"`
+	TeacherName string `gorm:"index"`
+	RoomNumber  int64
+	From        int64
+	To          int64
 }
