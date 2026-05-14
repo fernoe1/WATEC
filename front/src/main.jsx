771421f57c9e5a50697c2de5ee4d67a8bff45bf6
fernoe1 <krutoytemirlan2007@mail.ru>
2026-05-14 12:06:24 +0500

    stub(front)

---
+++ front/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Canvas } from '@react-three/fiber';
+import Experience from './Experience.jsx';
+import './index.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Canvas 
+        camera={ {
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [ 4, -2, 6 ]
+        } }
+    >
+      <Experience />
+    </Canvas>
+  </StrictMode>,
+);
